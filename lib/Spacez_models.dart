0838514fd9408f79494bb910153d2d1af0548c95
johnjahaziel <johnjahaziel00@gmail.com>
2026-02-18 11:29:23 +0530

    changes

---
+++ lib/Spacez_models.dart
@@ -0,0 +1,74 @@
+//property card - guests/bedroom/baths
+
+Padding(
+  padding: const EdgeInsets.only(left: 14.0, right: 12),
+  child: Row(
+    mainAxisSize: MainAxisSize.max,
+    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    children: [
+      Row(
+        children: [
+          SvgPicture.asset(
+            "assets/svg/guests.svg",
+            height: 20,
+          ),
+          4.hs(),
+          Text(
+            "<${widget.data.property.maxOccupancy} Guests",
+            style: inventoryStyle,
+          ),
+        ],
+      ),
+      
+      // Spacer(),
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Container(
+          height: 15,
+          width: 1,
+          color: Color(0xFF7D817D),
+        ),
+      ),
+      // Spacer(),
+
+      Row(
+        children: [
+          SvgPicture.asset(
+            "assets/svg/beds.svg",
+            height: 20,
+          ),
+          4.hs(),
+          Text(
+            "${widget.data.property.noOfBedrooms} Bedrooms",
+            style: inventoryStyle,
+          ),
+        ],
+      ),
+
+      // Spacer(),
+      Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Container(
+          height: 15,
+          width: 1,
+          color: Color(0xFF7D817D),
+        ),
+      ),
+      // Spacer(),
+
+      Row(
+        children: [
+          SvgPicture.asset(
+            "assets/svg/baths.svg",
+            height: 20,
+          ),
+          4.hs(),
+          Text(
+            "${widget.data.property.noOfBathrooms} Baths",
+            style: inventoryStyle,
+          ),
+        ],
+      ),
+    ],
+  ),
+),
